bb8bfc88d52a7754704722017438623e827d1243
Maksim Suhotskiy <Maksim.Suhotskiy@life.com.by>
2017-12-28 12:56:41 +0300

    Fix problems

--- CubicInterpolationGUI.m
+++ CubicInterpolationGUI.m
@@ -1,12 +1,12 @@
-classdef CubicInterpolationUI < matlab.apps.AppBase
+classdef CubicInterpolationGUI < matlab.apps.AppBase
     
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure              matlab.ui.Figure                   % Cubic in...
-        LabelEditField        matlab.ui.control.Label            % F(x) =
-        Fx                    matlab.ui.control.EditField        % 2*x^2+16/x
+        UIFigure              matlab.ui.Figure                   % UI Figure
         UIAxes                matlab.ui.control.UIAxes           % Graph
         BtnFindMin            matlab.ui.control.Button           % Find min
+        LabelEditField        matlab.ui.control.Label            % F(x) =
+        Fx                    matlab.ui.control.EditField        % 2*x^2+16/x
         Label4                matlab.ui.control.Label            % X  =
         X0                    matlab.ui.control.EditField        % 1
         Label5                matlab.ui.control.Label            %  =
@@ -24,13 +24,8 @@ classdef CubicInterpolationUI < matlab.apps.AppBase
     
     methods (Access = private)
         
-        % Code that executes after component creation
-        function startupFcn(app)
-            
-        end
-        
         % BtnFindMin button pushed function
-        function onBtnFindClicked(app)
+        function onBtnFindMinClick(app)
             
             fun = str2func(['@(x)' vectorize(app.Fx.Value)]);
             x1 = str2double(app.X0.Value);
@@ -38,12 +33,13 @@ classdef CubicInterpolationUI < matlab.apps.AppBase
             eps1 = str2double(app.Eps1.Value);
             eps2 = str2double(app.Eps2.Value);
             
-            xFinal = CubicInterpolation(fun, x1, delta, eps1, eps2);
+            xFinal = CubicInterpolation(app, x1, delta, eps1, eps2);
             
             %Show answer
             app.MinX.Value = double(xFinal);
             app.MinX.Enable = 'on';
             
+            syms x
             yFinal = subs(fun, x, xFinal);
             app.MinY.Value = double(yFinal);
             app.MinY.Enable = 'on';
@@ -57,7 +53,6 @@ classdef CubicInterpolationUI < matlab.apps.AppBase
             x = 0:0.01:5;
             
             plot(app.UIAxes, x, fun(x))
-            
         end
     end
     
@@ -69,20 +64,9 @@ classdef CubicInterpolationUI < matlab.apps.AppBase
             
             % Create UIFigure
             app.UIFigure = uifigure;
-            app.UIFigure.Position = [100 100 640 374];
-            app.UIFigure.Name = 'Cubic interpolation method';
+            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Name = 'UI Figure';
             setAutoResize(app, app.UIFigure, true)
-            
-            % Create LabelEditField
-            app.LabelEditField = uilabel(app.UIFigure);
-            app.LabelEditField.HorizontalAlignment = 'right';
-            app.LabelEditField.Position = [32 301 34 15];
-            app.LabelEditField.Text = 'F(x) = ';
-            
-            % Create Fx
-            app.Fx = uieditfield(app.UIFigure, 'text');
-            app.Fx.Position = [65 299 216 20];
-            app.Fx.Value = '2*x^2+16/x';
             
             % Create UIAxes
             app.UIAxes = uiaxes(app.UIFigure);
@@ -92,98 +76,105 @@ classdef CubicInterpolationUI < matlab.apps.AppBase
             app.UIAxes.Box = 'on';
             app.UIAxes.XGrid = 'on';
             app.UIAxes.YGrid = 'on';
-            app.UIAxes.Position = [307 103 300 247];
+            app.UIAxes.Position = [305 155 300 247];
             
             % Create BtnFindMin
             app.BtnFindMin = uibutton(app.UIFigure, 'push');
-            app.BtnFindMin.ButtonPushedFcn = createCallbackFcn(app, @onBtnFindClicked);
-            app.BtnFindMin.Position = [82 40 181 39];
+            app.BtnFindMin.ButtonPushedFcn = createCallbackFcn(app, @onBtnFindMin);
+            app.BtnFindMin.Position = [80 92 181 39];
             app.BtnFindMin.Text = 'Find min';
+            
+            % Create LabelEditField
+            app.LabelEditField = uilabel(app.UIFigure);
+            app.LabelEditField.HorizontalAlignment = 'right';
+            app.LabelEditField.Position = [30 353 34 15];
+            app.LabelEditField.Text = 'F(x) = ';
+            
+            % Create Fx
+            app.Fx = uieditfield(app.UIFigure, 'text');
+            app.Fx.Position = [63 351 216 20];
+            app.Fx.Value = '2*x^2+16/x';
             
             % Create Label4
             app.Label4 = uilabel(app.UIFigure);
             app.Label4.HorizontalAlignment = 'right';
-            app.Label4.Position = [37 265 29 15];
+            app.Label4.Position = [35 317 29 15];
             app.Label4.Text = 'X  = ';
             
             % Create X0
             app.X0 = uieditfield(app.UIFigure, 'text');
-            app.X0.Position = [65 263 216 20];
+            app.X0.Position = [63 315 216 20];
             app.X0.Value = '1';
             
             % Create Label5
             app.Label5 = uilabel(app.UIFigure);
             app.Label5.HorizontalAlignment = 'right';
-            app.Label5.Position = [45 226 21 15];
+            app.Label5.Position = [43 278 21 15];
             app.Label5.Text = ' = ';
             
             % Create Delta
             app.Delta = uieditfield(app.UIFigure, 'text');
-            app.Delta.Position = [65 224 216 20];
+            app.Delta.Position = [63 276 216 20];
             app.Delta.Value = '1';
             
             % Create Label6
             app.Label6 = uilabel(app.UIFigure);
             app.Label6.HorizontalAlignment = 'right';
-            app.Label6.Position = [41 189 25 15];
+            app.Label6.Position = [39 241 25 15];
             app.Label6.Text = ' = ';
             
             % Create Eps1
             app.Eps1 = uieditfield(app.UIFigure, 'text');
-            app.Eps1.Position = [65 186 216 20];
+            app.Eps1.Position = [63 238 216 20];
             app.Eps1.Value = '0.01';
             
             % Create Label7
             app.Label7 = uilabel(app.UIFigure);
             app.Label7.HorizontalAlignment = 'right';
-            app.Label7.Position = [41 151 25 15];
+            app.Label7.Position = [39 203 25 15];
             app.Label7.Text = ' = ';
             
             % Create Eps2
             app.Eps2 = uieditfield(app.UIFigure, 'text');
-            app.Eps2.Position = [65 149 216 20];
+            app.Eps2.Position = [63 201 216 20];
             app.Eps2.Value = '0.03';
             
             % Create LabelNumericEditField
             app.LabelNumericEditField = uilabel(app.UIFigure);
             app.LabelNumericEditField.HorizontalAlignment = 'right';
-            app.LabelNumericEditField.Position = [398 64 20 15];
+            app.LabelNumericEditField.Position = [396 116 20 15];
             app.LabelNumericEditField.Text = 'X =';
             
             % Create MinX
             app.MinX = uieditfield(app.UIFigure, 'numeric');
             app.MinX.Enable = 'off';
             app.MinX.Editable = 'off';
-            app.MinX.Position = [433 60 100 22];
+            app.MinX.Position = [431 112 100 22];
             
             % Create Label9
             app.Label9 = uilabel(app.UIFigure);
             app.Label9.HorizontalAlignment = 'right';
-            app.Label9.Position = [398 35 20 15];
+            app.Label9.Position = [396 87 20 15];
             app.Label9.Text = 'Y =';
             
             % Create MinY
             app.MinY = uieditfield(app.UIFigure, 'numeric');
             app.MinY.Enable = 'off';
             app.MinY.Editable = 'off';
-            app.MinY.Position = [433 31 100 22];
+            app.MinY.Position = [431 83 100 22];
         end
     end
     
     methods (Access = public)
         
         % Construct app
-        function app = CubicInterpolationUI()
+        function app = CubicInterpolationGUI()
             
             % Create and configure components
             createComponents(app)
             
             % Register the app with App Designer
             registerApp(app, app.UIFigure)
-            
-            % Execute the startup function
-            runStartupFcn(app, @startupFcn)
-            
             if nargout == 0
                 clear app
             end
