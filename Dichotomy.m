973a92cb643efc71527fbeabfc0fab5cc29056d5
Maksim Suhotskiy <Maksim.Suhotskiy@life.com.by>
2017-12-29 14:34:06 +0300

    Start implement Hooke-Jeeves with Cubic Interpolation

--- Dichotomy.m
+++ Dichotomy.m
@@ -1,8 +1,13 @@
 function min = Dichotomy(fun,x0,y0,ay,by,k,e)
 
 while(abs(by-ay)>e)
-    fx1=fun(x0+(((ay + by) / 2 + ay) / 2-y0)/k, ((ay + by) / 2 + ay) / 2);
-    fx2=fun(x0+(((ay + by) / 2 + by) / 2-y0)/k, ((ay + by) / 2 + by) / 2);
+    x1 = x0+(((ay + by) / 2 + ay) / 2-y0)/k;
+    x2 = ((ay + by) / 2 + ay) / 2;
+    fx1=fun(x1, x2);
+    
+    x1 = x0+(((ay + by) / 2 + by) / 2-y0)/k;
+    x2 = ((ay + by) / 2 + by) / 2;
+    fx2=fun(x1, x2);
     
     if (fx1<=fx2)
         by=(ay + by) / 2;
---
+++ HookeJeevesWithDichotomy.m
@@ -0,0 +1,135 @@
+function [x1, y1, x2, y2, x3, y3] = HookeJeevesWithDichotomy(fun, x0, y0, ax, bx, exp)
+ay=ax;
+by=bx;
+q=0.1;
+x3=x0;
+y3=y0;
+k3=1;
+
+while (exp >= q)
+    x0=x3;
+    y0=y3;
+    
+    x1=x0;
+    y1=y0;
+    flag=10;
+    while (exp>=q && (x1==x0) && (y1==y0))
+        if (fun(x0, y0) > fun(x0 + exp, y0 + exp*(k3-1)))
+            x1 = x0 + exp*k3;
+            y1 = y0 + exp*(k3-1);
+            flag=0;
+        else if (fun(x0, y0) > fun(x0 - exp*(k3-1), y0 + exp))
+                x1 = x0 - exp*(k3-1);
+                y1 = y0 + exp*k3;
+                flag=1;
+            else if (fun(x0, y0) > fun(x0 - exp, y0 - exp*(k3-1)))
+                    x1 = x0 - exp*k3;
+                    y1 = y0 - exp*(k3-1);
+                    flag=0;
+                else if (fun(x0, y0) > fun(x0 + exp*(k3-1), y0 - exp))
+                        x1 = x0 + exp*(k3-1);
+                        y1 = y0 - exp*k3;
+                        flag=1;
+                    else
+                        exp = exp / 2;
+                    end
+                end
+            end
+        end
+    end
+    
+    x2=x1;
+    y2=y1;
+    while (exp>=q && (x1==x2) && (y1==y2))
+        line([x0,x1],[y0,y1],'Color','r','LineWidth',1.5)
+        x2 = x1;
+        y2 = y1;
+        if (x0 ~= x1 && y0 == y1)
+            if (fun(x1, y1) > fun(x1, y1 + exp))
+                x2 = x1;
+                y2 = y1 + exp;
+            else if (fun(x1, y1) > fun(x1, y1 - exp))
+                    x2 = x1;
+                    y2 = y1 - exp;
+                else
+                    exp = exp / 2;
+                end
+            end
+        end
+        if (y0 ~= y1 && x0 == x1)
+            if (fun(x1, y1) > fun(x1 + exp, y1))
+                x2 = x1 + exp;
+                y2 = y1;
+            else if (fun(x1, y1) > fun(x1 - exp, y1))
+                    x2 = x1 - exp;
+                    y2 = y1;
+                else
+                    exp = exp / 2;
+                end
+            end
+        end
+        if (y0 ~= y1 && x0 ~= x1)
+            if (flag==0)
+                if (fun(x1, y1) > fun(x1 - exp*(k3-1), y1 - exp*k3))
+                    x2 = x1 - exp*(k3-1);
+                    y2 = y1 - exp*k3;
+                else if(fun(x1, y1) > fun(x1 + exp*(k3-1), y1 + exp*k3))
+                        x2 = x1 + exp*(k3-1);
+                        y2 = y1 + exp*k3;
+                    else
+                        exp = exp / 2;
+                    end
+                end
+            end
+            if (flag==1)
+                if (fun(x1, y1) > fun(x1 - exp*k3, y1 + exp*(k3-1)))
+                    x2 = x1 - exp*k3;
+                    y2 = y1 + exp*(k3-1);
+                else if(fun(x1, y1) > fun(x1 + exp*k3, y1 - exp*(k3-1)))
+                        x2 = x1 + exp*k3;
+                        y2 = y1 - exp*(k3-1);
+                    else
+                        exp = exp / 2;
+                    end
+                end
+            end
+        end
+    end
+    if (x2 ~= x1 || y2 ~= y1)
+        line([x1,x2],[y1,y2],'Color','g','LineWidth',1.5)
+        k=(y2-y0)/(x2-x0);
+        if((y2-y0)>0)
+            line([x0,(by-y0)/k+x0],[y0,by],'Color','b','LineWidth',1.5)
+        end
+        if((y2-y0)<0)
+            line([x0,(ay-y0)/k+x0],[y0,ay],'Color','b','LineWidth',1.5)
+        end
+    end
+    
+    e = 0.05;
+    if ((y2 - y0) > 0)
+        if (abs(k) >= 1)
+            y3=Dichotomy(fun,x0,y0,y0,by,k,e);
+            x3=x0+(y3-y0)/k;
+            k31=(y3 - y0)/sqrt((y3 - y0)*(y3 - y0)+(x3 - x0)*(x3 - x0));
+        else
+            x3=Dichotomy(fun,x0,y0,x0,bx,k,e);
+            y3=k*(x3 - x0)+y0;
+            k31=(y3 - y0)/sqrt((y3 - y0)*(y3 - y0)+(x3 - x0)*(x3 - x0));
+        end
+        
+    else
+        if (abs(k) >= 1)
+            y3=Dichotomy(fun,x0,y0,ay,y0,k,e);
+            x3=x0-(y0-y3)/k;
+            k31=(y3 - y0)/sqrt((y3 - y0)*(y3 - y0)+(x3 - x0)*(x3 - x0));
+        else
+            x3=Dichotomy(fun,x0,y0,ax,x0,k,e);
+            y3=y0-k*(x0-x3);
+            k31=(y3 - y0)/sqrt((y3 - y0)*(y3 - y0)+(x3 - x0)*(x3 - x0));
+        end
+    end
+    
+    k3 = k31;
+end
+end
