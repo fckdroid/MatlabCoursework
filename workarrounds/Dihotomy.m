9ecffa4013236b1c4b440e1b1ea387bf2ab6b4c7
Maksim Suhotskiy <Maksim.Suhotskiy@life.com.by>
2018-01-10 18:36:59 +0300

    Add workarrounds

---
+++ workarrounds/Dihotomy.m
@@ -0,0 +1,23 @@
+function min = Dihotomy(fun,x0,y0,ay,by,k,e)
+
+while(abs(by-ay)>e)
+    x1 = x0+(((ay + by) / 2 + ay) / 2-y0)/k;
+    x2 = ((ay + by) / 2 + ay) / 2;
+    fx1=fun(x1, x2);
+    
+    x1 = x0+(((ay + by) / 2 + by) / 2-y0)/k;
+    x2 = ((ay + by) / 2 + by) / 2;
+    fx2=fun(x1, x2);
+    
+    if (fx1<=fx2)
+        by=(ay + by) / 2;
+    else
+        ay=(ay + by) / 2;
+    end
+end
+
+min = (ay + by) / 2;
+
+end
+
+
